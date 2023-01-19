e325795d25c1989646f62f0ec549e59f36035963
livictor888 <https://github.com/livictor888/COMP3761>
2023-01-18 20:48:56 -0800

    change URL to render.com

---
+++ lab2 - Client-to-Server Communication/public/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+	$('#chatbot-form').submit(function (event) {
+		event.preventDefault();
+		const message = $('#chatbot-input').val();
+		$.ajax({
+			type: 'POST',
+			//url: 'https://lab2-cw70.onrender.com',
+			url: 'http://localhost:3000/chatbot',
+			data: {
+				message: message
+			},
+			success: function (response) {
+				let newMessage = $('<div>', {
+					class: 'message'
+				}).text(response.text);
+				let removeButton = $('<button>', {
+					class: 'remove-button'
+				}).text('Remove');
+				newMessage.append(removeButton);
+				$('#chat-history').append(newMessage);
+			}
+		});
+	});
+});
+
+$(document).on('click', '.remove-button', function() {
+	$(this).parent().remove();
+});
